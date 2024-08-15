149b72cd9b43e129ba5676a5efc59213a1463f39
Ingrid <ingrid.oa@gmail.com>
2024-08-14 23:29:18 -0400

    cambio

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-let app = document.getElementById('typewriter');
- 
+let app = document.getElementById("typewriter");
+
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
 });
- 
+
 typewriter
   .pauseFor(2500)
-  .typeString('Implementación de Tecnologías, Manejo de Equipos Multidiciplinarios y Desarrollo Web.')
+  .typeString(
+    "Implementación de Tecnologías, Manejo de Equipos Multidiciplinarios y Desarrollo Web."
+  )
   .pauseFor(200)
   .deleteChars(10)
   .start();
